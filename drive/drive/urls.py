78d4f40cbdbefaf720c5d7941018440068f932ce
Sai Maruthi <maruthisai92@gmail.com>
2021-08-18 16:33:36 +0530

    start new app Storage

--- drive/drive/urls.py
+++ drive/drive/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('authapp.urls', namespace='authapp')) 
+    path('auth/', include('authapp.urls', namespace='authapp')),
+    path('storage/', include('storage.urls', namespace='storage')),
 ]
 
 
