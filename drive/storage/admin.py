3fc6e4a4b26a34ed4e85f013009f3f3b13bbdc8e
Sai Maruthi <maruthisai92@gmail.com>
2021-08-18 17:31:37 +0530

    Create File & Folder Models

--- drive/storage/admin.py
+++ drive/storage/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import DriveFolder, DriveFile
 
 # Register your models here.
+
+
+admin.site.register(DriveFolder)
+admin.site.register(DriveFile)
--- drive/storage/models.py
+++ drive/storage/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.fields import files
 
 # Create your models here.
+
+
+def upload_file(instance,filename):
+    return f'uploads/{instance.owner.id}/{filename}'
+
+
+class DriveFolder(models.Model):
+    name            = models.CharField(max_length=255)
+    owner           = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='folder', on_delete=models.CASCADE)
+    parent          = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
+    created_on      = models.DateTimeField(auto_now_add=True)
+    updated_on      = models.DateTimeField(auto_now=True)
+
+    def str(self):
+        return self.name
+
+
+class DriveFile(models.Model):
+    file            = models.FileField(upload_to=upload_file, max_length=255)
+    owner           = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='file', on_delete=models.CASCADE)
+    parent          = models.ForeignKey(DriveFolder,null=True, on_delete=models.CASCADE)
+    created_on      = models.DateTimeField(auto_now_add=True)
+    updated_on      = models.DateTimeField(auto_now=True)
+
+    def str(self):
+        return self.file.name
