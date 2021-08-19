406e41dbca1d69d206c084da0e47c408d0ebcd12
Sai Maruthi <maruthisai92@gmail.com>
2021-08-20 02:05:18 +0530

    add validation to DriveFolderSerializer

--- drive/storage/api/serializers.py
+++ drive/storage/api/serializers.py
@@ -7,3 +7,12 @@ class DriveFolderSerializer(serializers.ModelSerializer):
         model = DriveFolder
         fields = ['id', 'name', 'file', 'is_folder', 'created_on', 'updated_on']
         
+    def validate(self, data):
+        is_folder = data.get('is_folder', None)
+        file = data.get('file', None)
+        name = data.get('name', None)
+        if is_folder and name == None:
+            raise serializers.ValidationError("Folder Name is Required")
+        elif not is_folder and file==None:
+            raise serializers.ValidationError("File is required")
+        return data
