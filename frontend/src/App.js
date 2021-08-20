a113f81b7908a596b51fc62741359f56ae0c16ac
Sai Maruthi <maruthisai92@gmail.com>
2021-08-21 01:15:06 +0530

    Start Frontend reactApp

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import CloudDrive from "./CloudDrive";
+
+function App() {
+  return (
+    <>
+      <CloudDrive />
+      <input type="test" />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/CloudDrive.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CloudDrive() {
+  return <div>Cloud Drive</div>;
+}
